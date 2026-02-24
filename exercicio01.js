44c34e6c7ee06ddbd3a5fac83ce3a6c0c21a6fec
Dieguinho1807 <diego.lemos@nave.org.br>
2026-02-24 11:07:49 -0300

    Atividade 2 de DG victor

--- exercicio01.js
+++ exercicio01.js
@@ -32,22 +32,22 @@
  * ser escrito entre áspas.
  */
 // Resposta
-/* 01 */ let nomeDoTipoTexto
-/* 02 */ let nomeDoTipoNumero
-/* 03 */ let nomeDoTipoBooleano
-/* 04 */ let nomeDoTipoIndefinido
-/* 05 */ let nomeDoTipoObjeto
+/* 01 */ let nomeDoTipoTexto = "string"
+/* 02 */ let nomeDoTipoNumero = "number"
+/* 03 */ let nomeDoTipoBooleano = "boolean"
+/* 04 */ let nomeDoTipoIndefinido = "undefined"
+/* 05 */ let nomeDoTipoObjeto = "object"
 
 /* Questões de 01 a 05
  * 
  * Atribua para as variáveis abaixo valores correspondentes ao tipo de cada uma.
  */
 // Resposta
-/* 06 */  let souTexto
-/* 07 */  let souNumero
-/* 08 */  let souBooleano 
-/* 09 */  let souIndefinido
-/* 10 */  let souObjeto
+/* 06 */  let souTexto = "Criatiano ronaldo é melhor que messi"
+/* 07 */  let souNumero = 7
+/* 08 */  let souBooleano = true
+/* 09 */  let souIndefinido = undefined
+/* 10 */  let souObjeto =  {}
 
 export {
   nomeDoTipoTexto,
