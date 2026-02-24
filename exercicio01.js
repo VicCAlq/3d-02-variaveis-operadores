a8655b916526a73773380afa765133d50bba73ad
Norcat27 <alan.silva@nave.org.br>
2026-02-24 11:16:20 -0300

    atividade feita pae

--- exercicio01.js
+++ exercicio01.js
@@ -32,22 +32,25 @@
  * ser escrito entre áspas.
  */
 // Resposta
-/* 01 */ let nomeDoTipoTexto
-/* 02 */ let nomeDoTipoNumero
-/* 03 */ let nomeDoTipoBooleano
-/* 04 */ let nomeDoTipoIndefinido
-/* 05 */ let nomeDoTipoObjeto
-
+/* 01 */ let nomeDoTipoTexto = "string"
+/* 02 */ let nomeDoTipoNumero = "number"
+/* 03 */ let nomeDoTipoBooleano = "boolean"
+/* 04 */ let nomeDoTipoIndefinido = "undefined"
+/* 05 */ let nomeDoTipoObjeto = {
+  constructor(nome){
+    nome = alano
+  }
+}
 /* Questões de 01 a 05
  * 
  * Atribua para as variáveis abaixo valores correspondentes ao tipo de cada uma.
  */
 // Resposta
-/* 06 */  let souTexto
-/* 07 */  let souNumero
-/* 08 */  let souBooleano 
-/* 09 */  let souIndefinido
-/* 10 */  let souObjeto
+/* 06 */  let souTexto = "3D já ganhou o interclasse!"
+/* 07 */  let souNumero = 27
+/* 08 */  let souBooleano = true
+/* 09 */  let souIndefinido = undefined
+/* 10 */  let souObjeto = ["a","b","c"]
 
 export {
   nomeDoTipoTexto,
--- exercicio02.js
+++ exercicio02.js
@@ -36,14 +36,14 @@
  * let cinquentaECinco = 5 * 11
  */
 // Respostas
-/* 01 */ let quarentaEDois = 7 ?? 6
-/* 02 */ let noventaENove = 198 ?? 2
-/* 03 */ let oitentaEQuatro = 44 ?? 40
-/* 04 */ let sessentaEOito = 340 ?? 5
-/* 05 */ let sessentaESeis = 6 ?? 11
-/* 06 */ let tres = 21 ?? 6
-/* 07 */ let centoEVinteEOito = 2 ?? 7
-/* 08 */ let setentaECinco = 5 ?? (3 ?? 5)
+/* 01 */ let quarentaEDois = 7 * 6
+/* 02 */ let noventaENove = 198 / 2
+/* 03 */ let oitentaEQuatro = 44 + 40
+/* 04 */ let sessentaEOito = 340 / 5
+/* 05 */ let sessentaESeis = 6 * 11
+/* 06 */ let tres = 21 % 6
+/* 07 */ let centoEVinteEOito = 2 ** 7
+/* 08 */ let setentaECinco = 5 * (3 * 5)
 /* 09 */ let centoEOito = 27 ?? 27 ?? 27 ?? 27
 /* 10 */ let cinquentaEUm = 10 ?? 5 ?? 1
 /* 11 */ let quinze = 90 ?? 6
